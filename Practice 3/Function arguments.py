2c86774f56f0fe9ca294129536ac8a9deba06bc2
YeldarAbdimalik <your_email@whatever.com>
2026-02-19 00:55:54 +0500

    task

--- Practice 3/Function arguments.py
+++ Practice 3/Function arguments.py
@@ -1,6 +1,6 @@
-def fahrenheit_to_celsius(fahrenheit):
-  return (fahrenheit - 32) * 5 / 9
+def my_function(fname):
+  print(fname + " Refsnes")
 
-print(fahrenheit_to_celsius(77))
-print(fahrenheit_to_celsius(95))
-print(fahrenheit_to_celsius(50))
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
