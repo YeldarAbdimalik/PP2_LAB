4a27d1571d77f011a737978b14b8e51e30702214
YeldarAbdimalik <your_email@whatever.com>
2026-02-20 19:26:49 +0500

    Add Practice4 - Python iterators, generators, dates, math, and JSON

---
+++ Practice 4/Python Math.py
@@ -0,0 +1,21 @@
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+z = math.sqrt(64)
+
+a = min(5, 10, 25)
+b = max(5, 10, 25)
+
+c = abs(-7.25)
+
+d = pow(4, 3)
+
+print(d) # returns 64
+print(c) # retunrs 7.25
+print(a) # returns 5
+print(b) # returns 25
+print(z) # returns 8
+print(x) # returns 2
+print(y) # returns 1
