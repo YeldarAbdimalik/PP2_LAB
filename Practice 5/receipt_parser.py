6be6b82e3cacdbda0b3050bb17a498146f17f6c5
YeldarAbdimalik <your_email@whatever.com>
2026-03-07 13:45:54 +0500

    Add Practice5 - Python RegEx and receipt parsing examples

--- Practice 5/receipt_parser.py
+++ Practice 5/receipt_parser.py
@@ -1,27 +1,27 @@
 import re
 import json
 
-# читаем файл
+
 with open("raw.txt", "r", encoding="utf-8") as f:
     text = f.read()
 
 
-# ---------- дата и время ----------
+
 date_time = re.search(r"Время:\s*([\d\.]+\s[\d:]+)", text)
 date_time = date_time.group(1) if date_time else None
 
 
-# ---------- способ оплаты ----------
+
 payment = re.search(r"(Банковская карта|Наличные)", text)
 payment = payment.group(1) if payment else None
 
 
-# ---------- итоговая сумма ----------
+
 total = re.search(r"ИТОГО:\s*\n([\d\s,]+)", text)
 total = total.group(1).strip() if total else None
 
 
-# ---------- товары ----------
+
 pattern = r"\d+\.\n(.+?)\n([\d,]+)\s*x\s*([\d\s,]+)\n([\d\s,]+)"
 
 items = []
@@ -37,7 +37,6 @@ for match in re.findall(pattern, text):
     })
 
 
-# ---------- итоговый JSON ----------
 receipt = {
     "datetime": date_time,
     "payment_method": payment,
@@ -46,7 +45,7 @@ receipt = {
 }
 
 
-# сохраняем JSON файл
+
 with open("receipt.json", "w", encoding="utf-8") as f:
     json.dump(receipt, f, indent=4, ensure_ascii=False)
 
